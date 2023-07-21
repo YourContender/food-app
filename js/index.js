2ecf9515a0f4ceca59d7c1dc2c4281f2a5bdc07e
YourContender <dream-boy123@ukr.net>
2023-07-22 01:52:41 +0300

    add rest operator

--- js/index.js
+++ js/index.js
@@ -126,12 +126,15 @@ window.addEventListener('DOMContentLoaded', () => {
     // const modalTimerId = setTimeout(openModalWindow, 5000);
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(
+            src, alt, title, descr, price, parentSelector, ...classes
+        ) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.transfer = 37.5;
             this.parent = document.querySelector(parentSelector);
             this.changeToUAH();
@@ -143,9 +146,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
-            element.classList.add('menu_item');
+
+            if (this.classes.length === 0) {
+                this.elem = 'menu_item';
+                element.classList.add(this.elem);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">
@@ -158,7 +167,6 @@ window.addEventListener('DOMContentLoaded', () => {
                         <span>${this.price}</span> грн/день
                     </div>
                 </div>
-            </div>
             `
 
             this.parent.append(element);
@@ -173,7 +181,8 @@ window.addEventListener('DOMContentLoaded', () => {
         Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и 
         высоким качеством!`,
         "9",
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -184,7 +193,8 @@ window.addEventListener('DOMContentLoaded', () => {
         исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в 
         ресторан!`,
         "12",
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -195,7 +205,8 @@ window.addEventListener('DOMContentLoaded', () => {
         животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное 
         количество белков за счет тофу и импортных вегетарианских стейков. `,
         "10",
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
 })
