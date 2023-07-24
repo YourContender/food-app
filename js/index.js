26d4ce0cf7413078212e3b7866665a4c924d0ad5
YourContender <dream-boy123@ukr.net>
2023-07-24 18:54:25 +0300

    add request server xmlhttprequest

--- js/index.js
+++ js/index.js
@@ -209,4 +209,54 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item'
     ).render();
 
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо, мы скоро с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const obj = {}
+
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(json);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000)
+                }
+            });
+        })
+    }
 })
